9c25a72ae8b3c1749d10218a0fd75e28351c2307
islomjon17 <turgunovislomjon107@gmail.com>
2024-10-24 09:42:48 +0500

    updated

--- onshop/estore/estore/urls.py
+++ onshop/estore/estore/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from . import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("market.urls")),
-]
+] + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
--- onshop/estore/market/models.py
+++ onshop/estore/market/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name =models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    description = models.CharField(max_length=255, default='', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE , default=1, related_name='products')
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to="uloades/products/")
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    pustomer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone =  models.CharField(max_length=50, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+    def __str__(self):
+        return self.product
