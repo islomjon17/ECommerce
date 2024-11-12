18a6d9420073720f48613462af79a882e0283b97
islomjon17 <turgunovislomjon107@gmail.com>
2024-11-12 21:02:33 +0500

    updated

--- onshop/estore/shop1/views.py
+++ onshop/estore/shop1/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import Product
+from .forms import SignUpForm
 # Create your views here.
 
 
@@ -40,3 +45,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You heve been Loged out!!!!"))
     return redirect("home")
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            
+            user  = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have been joined to our market!!!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error, please try again!!!"))
+            return redirect('register')
+    return render(request, 'registration/registration.html', {'form':form})
